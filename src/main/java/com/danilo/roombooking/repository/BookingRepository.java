2c5c55c5e67ed617b40f35ea514e44e1ea04690c
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-02-27 22:25:53 -0300

    feat: add BookingRepository

---
+++ src/main/java/com/danilo/roombooking/repository/BookingRepository.java
@@ -0,0 +1,38 @@
+package com.danilo.roombooking.repository;
+
+import com.danilo.roombooking.domain.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+
+public interface BookingRepository extends JpaRepository<Booking, Long>, JpaSpecificationExecutor<Booking> {
+    Page<Booking> findByUserId(Long userId, Pageable pageable);
+    Page<Booking> findByRoomId(Long roomId, Pageable pageable);
+
+    @Query("""
+    SELECT COUNT(b) > 0 FROM Booking b
+    WHERE b.room.id = :roomId
+    AND b.startTime < :endTime
+    AND b.endTime > :startTime
+    """)
+    boolean isRoomBookedDuringTimeRange(@Param("roomId") Long roomId,
+                                        @Param("startTime") Timestamp startTime,
+                                        @Param("endTime") Timestamp endTime);
+
+    @Query("""
+    SELECT COUNT(b) > 0 FROM Booking b
+    WHERE b.room.id = :roomId
+    AND ((b.startTime BETWEEN :startTime AND :endTime)
+        OR (b.endTime BETWEEN :startTime AND :endTime))
+    AND b.id != :bookingId
+    """)
+    boolean isRoomBookedDuringTimeRangeExcludingCurrentBooking(@Param("roomId") Long roomId,
+                                       @Param("bookingId") Long bookingId,
+                                       @Param("startTime") Timestamp startTime,
+                                       @Param("endTime") Timestamp endTime);
+}
