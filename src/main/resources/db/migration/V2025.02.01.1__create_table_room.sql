4a7da0148bdb0f4f101738165e4caf19e3eebd7f
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-02-28 11:53:48 -0300

    refactor: capacity should be greater than zero

--- src/main/resources/db/migration/V2025.02.01.1__create_table_room.sql
+++ src/main/resources/db/migration/V2025.02.01.1__create_table_room.sql
@@ -4,7 +4,7 @@ CREATE TABLE Room
     Identifier          VARCHAR(50)     NOT NULL UNIQUE,
     Name                VARCHAR(50)     NOT NULL,
     Description         VARCHAR(100),
-    Capacity            INT             NOT NULL,
+    Capacity            INT             NOT NULL CHECK (Capacity > 0),
     Status              VARCHAR(50)     NOT NULL,
     Type                VARCHAR(50)     NOT NULL,
     CreatedAt           TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
