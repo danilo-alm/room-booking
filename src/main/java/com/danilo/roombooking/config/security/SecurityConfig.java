073a5a304353e2c1037ada1884dd3857481012f7
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-03-07 12:07:10 -0300

    feat: loose security config for development

---
+++ src/main/java/com/danilo/roombooking/config/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.danilo.roombooking.config.security;
+
+
+import com.danilo.roombooking.domain.role.RoleType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("dev")
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+            .requestMatchers("/**").permitAll()
+            .anyRequest().authenticated()
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy(
+            RoleType.ROLE_ADMIN.name() + ">" + RoleType.ROLE_MANAGER.name() + "\n" +
+            RoleType.ROLE_MANAGER.name() + ">" + RoleType.ROLE_USER
+        );
+    }
+
+}
